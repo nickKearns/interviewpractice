7db1ddefb436fc131c7b492c59b11d4bbbb07e74
nickKearns <nickkearns133@gmail.com>
2020-04-12 17:19:46 -0400

    another problem

---
+++ spdapril8th.py
@@ -0,0 +1,39 @@
+#find the longest substring of unique letters within a string of n letters
+#questions: does casing count as different letters?
+#what if two substrings are of equal length and are the longest?
+#Assume i can return the first substring, or either
+#assume casing counts as different letters
+#thinking of using a dictionary
+
+def find_substring(word):
+    longest_substring = ''
+
+    dict = {}
+    for i in range(len(word)):
+
+        counter = i
+        built_substring = ''
+
+        while word[counter] not in dict:
+
+            dict[word[counter]] = 1
+            
+            built_substring += word[counter]
+
+            if counter < len(word):
+                counter += 1
+            else:
+                break
+
+        if len(built_substring) > len(longest_substring):
+            longest_substring = built_substring
+    return longest_substring
+
+if __name__ == "__main__":
+
+    sample_str = 'ahgotnauhhtuyowl'
+    print(find_substring(sample_str))
+
+
+
+
---
+++ sum_question.py
@@ -0,0 +1,23 @@
+#given an array and a target value find two numbers that sum to the target value
+#what if there are more than 1 combination of numbers that add up to the target
+#return the first found combo
+#assume the list is not ordered
+#[1,5,8,4,10] -> 12
+#store all the complements in a dictionary and then with each 
+#iteration over the list check if that number exists in the built dictionary
+
+
+def find_target(arr, target):
+    seen_comps = {}
+    for num in arr:
+        complement = target - num
+        if complement in seen_comps:
+            return (complement, num)
+        else:
+            seen_comps[num] = 1
+
+
+if __name__ == "__main__":
+
+    test_list = [1,5,8,4,10]
+    print(find_target(test_list, 12))
